3a097820e0d6f7ceb9dce56bd1aebb2d2210aa71
Junki Hori <junki326+github@gmail.com>
2021-02-06 16:56:46 +0900

    第6回 useState 変数、配列、オブジェクトを扱ってみよう

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter';
-import CounterHook from './components/CounterHook';
+// import Counter from './components/Counter';
+// import CounterHook from './components/CounterHook';
+// import FormHook from  './components/FormHook';
+import ItemHook from './components/ItemHook';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <CounterHook />
+      {/* <Counter />
+      <CounterHook /> */}
+      {/* <FormHook /> */}
+      <ItemHook />
     </div>
   );
 }
